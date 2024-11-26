a93211f30a40095548744ada292c786223610b39
Janatã Aruanda Silva Barbosa <78488269+Janata-Aruanda@users.noreply.github.com>
2024-11-26 08:53:46 -0300

    Update extract.py

--- scr/extract.py
+++ scr/extract.py
@@ -2,6 +2,7 @@ import json
 import os
 
 def desmembrar_json(arquivo_json, saida_base):
+    
     # Verifica se o diretório de saída existe, caso contrário, cria
     os.makedirs(saida_base, exist_ok=True)
 
@@ -9,18 +10,16 @@ def desmembrar_json(arquivo_json, saida_base):
         data = json.load(f)
 
     for guest_check in data['guestChecks']:
-        # Informações básicas do cheque
         cheque_basico = {k: v for k, v in guest_check.items() if k not in ['taxes', 'detailLines']}
         with open(f"{saida_base}/guestChecks.json", 'w') as f:
             json.dump(cheque_basico, f, indent=4)
 
-        # Informações dos impostos
+        # Informações dos taxes
         with open(f"{saida_base}/taxes.json", 'w') as f:
             json.dump(guest_check['taxes'], f, indent=4)
 
-        # Informações das linhas de detalhe e itens do menu
         for idx, detail_line in enumerate(guest_check['detailLines']):
-            # Remove a chave 'menuItem' para salvar somente os detalhes
+            # Remover'menuItem' para salvar somente os details
             detail_line_without_menu_item = {k: v for k, v in detail_line.items() if k != 'menuItem'}
             with open(f"{saida_base}/detailLines.json", 'w') as f:
                 json.dump(detail_line_without_menu_item, f, indent=4)
@@ -31,7 +30,6 @@ def desmembrar_json(arquivo_json, saida_base):
                     json.dump(detail_line['menuItem'], f, indent=4)
 
 if __name__ == "__main__":
-    # Exemplo de uso
     arquivo_entrada = "ERP.json"
     pasta_saida = "data/raw"
     desmembrar_json(arquivo_entrada, pasta_saida)
